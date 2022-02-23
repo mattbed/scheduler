6c7ff98986faabc2e4eb5f83b516119fc7894602
mattbed <m.bednarski@gmail.com>
2022-02-23 15:30:21 -0500

    Add InterviewerList states/props functionality

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -1,12 +1,24 @@
 import React from 'react';
+import InterviewListItem from "./InterviewerListItem";
 
 import "./InterviewerList.scss";
 
 export default function InterviewerList(props) {
+  const parsedInterviewerList = props.interviewers.map((element) => 
+  <InterviewListItem 
+  id={element.id}
+  name={element.name}
+  avatar={element.avatar}
+  selected={(props.interviewer === element.id) ? true : false}
+  setInterviewer={props.setInterviewer}
+  />)
+  
   return (
   <section className="interviewers">
     <h4 className="interviewers__header text--light">Interviewer</h4>
-    <ul className="interviewers__list"></ul>
+    <ul className="interviewers__list">
+      {parsedInterviewerList}
+    </ul>
   </section>
   )
 }
