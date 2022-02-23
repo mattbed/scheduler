1dd214533aeb132d066e15e1ac3fec9aabd26b4d
mattbed <m.bednarski@gmail.com>
2022-02-22 23:34:32 -0500

    Add styles for DayListItems

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -1,8 +1,13 @@
 import React from "react";
 import classNames from "classnames";
 
+import "./DayListItem.scss";
+
 export default function DayListItem(props) {
-  let fullClass = classNames({'.day-list__item--full': !props.spots })
+  let fullClass = classNames('day-list__item', 
+    { 'day-list__item--full': !props.spots },
+    { 'day-list__item--selected': props.selected}
+    )
   const setDayVar = () => {
     props.setDay(props.name);
   }
