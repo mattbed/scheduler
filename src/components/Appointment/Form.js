ec2cf8af9bd519f450a01edb2829696b8ea8c3ca
mattbed <m.bednarski@gmail.com>
2022-02-24 00:17:34 -0500

    Add form component

---
+++ src/components/Appointment/Form.js
@@ -0,0 +1,35 @@
+import React from 'react';
+
+import Button from "../Button.js";
+import InterviewerList from "../InterviewerList.js";
+
+
+export default function(props) {
+  return (
+    <main className="appointment__card appointment__card--create">
+  <section className="appointment__card-left">
+    <form autoComplete="off">
+      <input
+        className="appointment__create-input text--semi-bold"
+        name="name"
+        type="text"
+        placeholder="Enter Student Name"
+        /*
+          This must be a controlled component
+          your code goes here
+        */
+      />
+    </form>
+    <InterviewerList 
+      /* your code goes here */
+    />
+  </section>
+  <section className="appointment__card-right">
+    <section className="appointment__actions">
+      <Button danger onClick={props.onCancel}>Cancel</Button>
+      <Button confirm onClick={props.onSave}>Save</Button>
+    </section>
+  </section>
+</main>
+  )
+}
--- stories/index.js
+++ stories/index.js
@@ -17,6 +17,7 @@ import Show from "components/Appointment/Show.js";
 import Confirm from "components/Appointment/Confirm.js";
 import Status from "components/Appointment/Status.js";
 import Error from "components/Appointment/Error.js";
+import Form from "components/Appointment/Form.js";
 
 storiesOf("Button", module)
   .addParameters({
@@ -173,5 +174,21 @@ storiesOf("Button", module)
     <Error
     message="Could not delete appointment."
     onClose={action("onClose")}
+    />
+  ))
+  .add("Edit Form", () => (
+    <Form
+    student="Wort"
+    interviewer="1"
+    interviewers={interviewers}
+    onSave={action("onSave")}
+    onCancel={action("onCancel")}
+    />
+  ))
+  .add("Create Form", () => (
+    <Form
+    interviewers={interviewers}
+    onSave={action("onSave")}
+    onCancel={action("onCancel")}
     />
   ));
