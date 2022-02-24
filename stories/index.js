6d5299f3c83140905e09f31460c6ef0601e6bc4d
mattbed <m.bednarski@gmail.com>
2022-02-23 22:16:02 -0500

    Add components to Appointment

--- stories/index.js
+++ stories/index.js
@@ -15,6 +15,8 @@ import Header from "components/Appointment/Header.js";
 import Empty from "components/Appointment/Empty.js";
 import Show from "components/Appointment/Show.js";
 import Confirm from "components/Appointment/Confirm.js";
+import Status from "components/Appointment/Status.js";
+import Error from "components/Appointment/Error.js";
 
 storiesOf("Button", module)
   .addParameters({
@@ -164,5 +166,12 @@ storiesOf("Button", module)
     message="Delete the appointment?"
     onConfirm={action("onConfirm")}
     onCancel={action("onCancel")}
+    />
+  ))
+  .add("Status", () => <Status message="Deleting"/>)
+  .add("Error", () => (
+    <Error
+    message="Could not delete appointment."
+    onClose={action("onClose")}
     />
   ));
