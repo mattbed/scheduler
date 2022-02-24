8005470803bb3e32469bba6c3d6eab7a6cc28a9c
mattbed <m.bednarski@gmail.com>
2022-02-23 22:07:22 -0500

    Add components to Appointment

--- stories/index.js
+++ stories/index.js
@@ -13,6 +13,8 @@ import InterviewerList from "components/InterviewerList";
 import Appointment from "components/Appointment/index.js";
 import Header from "components/Appointment/Header.js";
 import Empty from "components/Appointment/Empty.js";
+import Show from "components/Appointment/Show.js";
+import Confirm from "components/Appointment/Confirm.js";
 
 storiesOf("Button", module)
   .addParameters({
@@ -147,5 +149,20 @@ storiesOf("Button", module)
   .add("Empty", () => (
     <Empty 
     onAdd={action("onAdd")}
+    />
+  ))
+  .add("Show", () => (
+    <Show
+    student="Lydia Miller-Jones"
+    interviewer={interviewer}
+    onEdit={action("onEdit")}
+    onDelete={action("onDelete")}
+    />
+  ))
+  .add("Confirm", () => (
+    <Confirm
+    message="Delete the appointment?"
+    onConfirm={action("onConfirm")}
+    onCancel={action("onCancel")}
     />
   ));
